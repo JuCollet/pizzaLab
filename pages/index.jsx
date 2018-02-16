3eff7fc0da282f52b47ddc058c0ef70ffdeae87b
Julien <ju.collet@gmail.com>
2018-02-16 10:04:44 +0100

    initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Index = () => (
+  <div>
+    <h1>C'est bon mec</h1>
+  </div>
+)
+
+export default Index;
---
+++ test/pages/index.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react'; // eslint-disable-line no-unused-vars
+import { mount, shallow } from 'enzyme';
+import { expect } from 'chai';
+
+import Index from '../../pages/index';
+
+describe('App', () => {
+  it('exists', () => {
+    //const wrapper = shallow(<Index />);
+    expect(true).to.eql(true);
+  });
+});
