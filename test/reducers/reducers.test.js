f4753068b3eb5c3520a004ed1c0d0e260e072e33
Julien <ju.collet@gmail.com>
2018-03-05 08:52:08 +0100

    Update reducers tests

--- test/reducers/reducers.test.js
+++ test/reducers/reducers.test.js
@@ -6,12 +6,13 @@ import fetchingReducer from '../../reducers/reducer_fetching';
 describe('Reducers', () => {
   const initialState = {
     toppings: {},
-    selection: { price: 9 },
+    selection: { price: 9, anchoix: { amount: 2 } },
   };
   describe('Configurator reducer', () => {
     it('Should return empty state if no state provided', () => {
+      const emptyState = { toppings: {}, selection: { price: 9 } };
       const newState = configuratorReducer(undefined, { type: 'ijijqj' });
-      expect(newState).to.eql(initialState);
+      expect(newState).to.eql(emptyState);
     });
     it('Should return untouched state if action doesnt match', () => {
       const newState = configuratorReducer(initialState, { type: 'ijijqj' });
@@ -24,12 +25,39 @@ describe('Reducers', () => {
       const newState = configuratorReducer(existingState, newAction);
       expect(newState.toppings).to.include.all.keys('name a', 'name b');
     });
+
     describe('updateSelectionPrice', () => {
       it('Should not throw an error if topping doesnt exists anymore', () => {
-        // If reducer try to get the price of an unknow topping in state.toppings, return current price;
+        // If reducer try to get the price of an unknow topping
+        // in state.toppings, return current price;
         const newAction = { type: SELECTION_UPDATE, payload: { name: 'ananas' } };
         const newState = configuratorReducer(initialState, newAction);
         expect(newState.selection.price).to.eql(9);
+      });
+      it('Should update the price correctly', () => {
+        const existingState = { toppings: { ananas: { name: 'name 1' }, anchoix: { name: 'name 2', fields: { price: 0.5 } } }, selection: { price: 9, anchoix: { amount: 1 } } };
+        const newAction = { type: SELECTION_UPDATE, payload: { name: 'anchoix', increment: true } };
+        const newAction2 = { type: SELECTION_UPDATE, payload: { name: 'anchoix', increment: false } };
+        const newState = configuratorReducer(existingState, newAction);
+        const newState2 = configuratorReducer(existingState, newAction2);
+        expect(newState.selection.price).to.eql(9.5);
+        expect(newState2.selection.price).to.eql(8.5);
+      });
+    });
+
+    describe('updateSelectionAmount', () => {
+      it('Should increment the amount if action.payload.increment is true', () => {
+        const newAction = { type: SELECTION_UPDATE, payload: { name: 'anchoix', increment: true } };
+        const newState = configuratorReducer(initialState, newAction);
+        expect(newState.selection.anchoix.amount).to.eql(3);
+      });
+      it('Should decrement the amount if action.payload.increment is false or undefined', () => {
+        const newAction = { type: SELECTION_UPDATE, payload: { name: 'anchoix', increment: false } };
+        const newAction2 = { type: SELECTION_UPDATE, payload: { name: 'anchoix' } };
+        const newState = configuratorReducer(initialState, newAction);
+        const newState2 = configuratorReducer(initialState, newAction2);
+        expect(newState.selection.anchoix.amount).to.eql(1);
+        expect(newState2.selection.anchoix.amount).to.eql(1);
       });
     });
   });
