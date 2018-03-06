01a214dbc57d7cbd19c5bee5780d440356793139
Julien <ju.collet@gmail.com>
2018-03-06 14:14:37 +0100

    Add ToppingItem description component

--- components/ToppingList/ToppingItem/ToppingItem.jsx
+++ components/ToppingList/ToppingItem/ToppingItem.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import withRedux from 'next-redux-wrapper';
 import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
@@ -8,14 +8,32 @@ import { updateSelection } from '../../../actions/configurator';
 import store from '../../../store';
 import Counter from '../../Counter/Counter';
 
-const StyledToppingItem = styled.li`
+const StyledDescriptionWrapper = styled.div`
+  overflow: hidden;
+  height: ${props => (props.isDescriptionHidden ? '0px' : 'initial')};
+  background-color: rgba(1,1,1,.5);
+  padding-bottom: ${props => (props.isDescriptionHidden ? '0px' : '12px')};
+  border-radius: 0px 0px 4px 4px;
+  transition: .2s;
+`;
+
+const Description = styled.span`
+  font-family: Roboto;
+  font-size: .9em;
+  font-weight: 300;
+  margin-left: 12px;
+  opacity: ${props => (props.isDescriptionHidden ? 0 : 1)};
+  transition: .2s;
+  color: #FFFFFF;
+`;
+
+const StyledIngredientWrapper = styled.div`
   display: flex;
   position: relative;
   align-items: center;
-  margin-bottom: 10px;
   padding: 12px;
   background-color: rgba(1,1,1,.5);
-  border-radius: 4px;
+  border-radius: ${props => (props.isDescriptionHidden ? '4px' : '4px 4px 0px 0px')};
 `;
 
 const Ingredient = styled.span`
@@ -24,19 +42,49 @@ const Ingredient = styled.span`
   color: #FFFFFF;
 `;
 
-const ToppingItem = props => (
-  <StyledToppingItem>
-    <Ingredient>{props.name}</Ingredient>
-    <Counter
-      amount={props.amount}
-      max={props.max}
-      name={props.name}
-      clickHandler={props.updateSelection}
-    />
-  </StyledToppingItem>
-);
+const StyledToppingItem = styled.li`
+  margin-bottom: 10px;
+`;
+
+class ToppingItem extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDescriptionHidden: true,
+    };
+    this.descriptionToggle = this.descriptionToggle.bind(this);
+  }
+
+  descriptionToggle() {
+    this.setState({
+      isDescriptionHidden: !this.state.isDescriptionHidden,
+    });
+  }
+
+  render() {
+    return (
+      <StyledToppingItem>
+        <StyledIngredientWrapper isDescriptionHidden={this.state.isDescriptionHidden} >
+          <Ingredient onClick={this.descriptionToggle}>{this.props.name}</Ingredient>
+          <Counter
+            amount={this.props.amount}
+            max={this.props.max}
+            name={this.props.name}
+            clickHandler={this.props.updateSelection}
+          />
+        </StyledIngredientWrapper>
+        <StyledDescriptionWrapper isDescriptionHidden={this.state.isDescriptionHidden} >
+          <Description isDescriptionHidden={this.state.isDescriptionHidden} >
+            &quot;{this.props.description}&quot;
+          </Description>
+        </StyledDescriptionWrapper>
+      </StyledToppingItem>
+    );
+  }
+}
 
 ToppingItem.propTypes = {
+  description: PropTypes.string,
   name: PropTypes.string.isRequired,
   updateSelection: PropTypes.func.isRequired,
   amount: PropTypes.number,
@@ -45,6 +93,7 @@ ToppingItem.propTypes = {
 
 ToppingItem.defaultProps = {
   amount: 0,
+  description: 'Aucune description',
   max: 3,
 };
 
